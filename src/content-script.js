d194eb8de462760eb44b5244125741b21f5e7b0d
Yuanzhuoxin <1974269607@qq.com>
2025-06-11 23:12:29 +0800

    chore: 调整插件结构

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,5 @@
 const target = document.createElement("script");
 target.type = "text/javascript";
-target.src = chrome.runtime.getURL("main.tsx");
+target.src = chrome.runtime.getURL("downloader/main.js");
 
 document.documentElement.appendChild(target);
--- src/downloader/main.tsx
+++ src/downloader/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
-import Downloader from "./downloader";
+import DownloaderComponent from "./DownloaderComponent";
 import "./index.css";
 
 const container = document.createElement("div");
 const body = document.querySelector("body")!;
 body.append(container);
 
-ReactDOM.createRoot(container).render(<Downloader />);
+ReactDOM.createRoot(container).render(<DownloaderComponent />);
